4f85587d838fcfe92adaa6576b084258773068a7
Alvin Utama <alvin.wardhana@aux.go-jek.com>
2018-03-11 14:38:07 +0700

    edit nama

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -79,7 +79,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel
+                    Alvin  Wardhana Utama oo
                 </div>
 
                 <div class="links">
